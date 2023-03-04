034814ede46af1408e5fe5b485420ee385db38c9
Abdi-phantom <126761442+Abdi-phantom@users.noreply.github.com>
2023-03-04 09:22:34 +0000

    Update GitPHubPractice2.java
    These changes are from upstream git repo

--- src/test/git/GitPHubPractice2.java
+++ src/test/git/GitPHubPractice2.java
@@ -5,5 +5,11 @@ public class GitPHubPractice2 {
 	public int test(int a, int b) {
 		return a*b;
 	}
+	
+	public int test(int x, int y, String message){
+		System.out.println(message);
+		int z= x+y;
+		return z;
+	}
 
-}
+} 
